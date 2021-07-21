80b1f240a15367ffdb1cabdf5fffe063b8a21828
Ashton Huxtable <aehuxtable@gmail.com>
2021-07-21 11:16:35 -0600

    Add tests for form inputs

--- cypress/integration/mainPage_spec.js
+++ cypress/integration/mainPage_spec.js
@@ -2,4 +2,23 @@ describe('Main page user flow', () => {
     beforeEach(() => {
         cy.visit('http://localhost:3000/')
     })
+
+    it('Should display a title', () => {
+        cy.contains('Turing Cafe Reservations')
+    })
+
+    it('Should display a form to make reservations', () => {
+        cy.get('input[name="name"]')
+            .type('Ashton')
+            .should('have.value', 'Ashton')
+            .get('input[name="date"]')
+            .type('07/21')
+            .should('have.value', '07/21')
+            .get('input[name="time"]')
+            .type('6:30')
+            .should('have.value', '6:30')
+            .get('input[name="number"]')
+            .type('{uparrow}')
+            .should('have.value', '1')
+    })
 })
