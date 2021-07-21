00b004498583da064c68ce0f3c02cfa7dac4fbb2
Ashton Huxtable <aehuxtable@gmail.com>
2021-07-21 12:08:12 -0600

    Create post request

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,5 +3,21 @@ const getAllReservations = () => {
         .then(response => response.json())
 }
 
+const addNewReservation = ({name, date, time, number}) => {
+    return fetch('http://localhost:3001/api/v1/reservations', {
+        method: 'POST',
+        body: JSON.stringify({
+            'name': name,
+            'date': date,
+            'time': time,
+            'number': number
+        }),
+        headers:{
+             'Content-Type' : 'application/json'
+        }
+    })
+    .then(response => response.json())
+}
 
-export { getAllReservations }
+
+export { getAllReservations, addNewReservation }
