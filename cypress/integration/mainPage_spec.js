e45dd89cbb00b17964ecad2ae58a499865f0f0aa
Ashton Huxtable <aehuxtable@gmail.com>
2021-07-21 11:36:55 -0600

    Create stub/intercept and test for existing reservation

--- cypress/integration/mainPage_spec.js
+++ cypress/integration/mainPage_spec.js
@@ -1,5 +1,12 @@
 describe('Main page user flow', () => {
     beforeEach(() => {
+        cy.fixture('reservations').then((reservations) => {
+            const baseUrl = 'http://localhost:3001/api/v1/';
+            cy.intercept('GET', `${baseUrl}/reservations`, {
+                statusCode: 200,
+                body: reservations
+            })
+        })
         cy.visit('http://localhost:3000/')
     })
 
@@ -7,7 +14,13 @@ describe('Main page user flow', () => {
         cy.contains('Turing Cafe Reservations')
     })
 
-    it('Should display a form to make reservations', () => {
+    it('Should display reservations', () => {
+        cy.get('article')
+            .contains('Christie')
+            .get('button').contains('Cancel')
+    })
+
+    it('Should display a form to make reservations and allow a user to add reservations', () => {
         cy.get('input[name="name"]')
             .type('Ashton')
             .should('have.value', 'Ashton')
@@ -20,9 +33,7 @@ describe('Main page user flow', () => {
             .get('input[name="number"]')
             .type('{uparrow}')
             .should('have.value', '1')
+            .get('button').contains('Make Reservation').click();
     })
 
-    it('Should be able to submit the form', () => {
-        cy.get('button').contains('Make Reservation').click();
-    })
 })
