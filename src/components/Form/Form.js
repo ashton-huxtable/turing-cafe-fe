68b8f65cf8dc2c515f7612ab62aba36719d60851
Ashton Huxtable <aehuxtable@gmail.com>
2021-07-21 10:10:21 -0600

    Create class component for Form and export

---
+++ src/components/Form/Form.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import './Form.css';
+
+class Form extends Component {
+    constructor() {
+        super();
+    }
+
+    render(){
+        return(
+            <form>
+                
+            </form>
+        )
+    }
+}
+
+export default Form;
