33bf6174c7304ac5dc307d5c90f7330d8a5b8886
Ashton Huxtable <aehuxtable@gmail.com>
2021-07-21 09:55:55 -0600

    Fetch all reservations and set state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,3 +1,4 @@
+import { getAllReservations } from '../../apiCalls';
 import React, { Component } from 'react';
 import Reservations from '../Reservations/Reservations';
 import './App.css';
@@ -6,31 +7,18 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      reservations: [
-        {
-          "id": 1,
-          "name": "Christie",
-          "date": "12/29",
-          "time": "7:00",
-          "number": 12
-          },
-          {
-          "id": 2,
-          "name": "Leta",
-          "date": "4/5",
-          "time": "7:00",
-          "number": 2
-          },
-          {
-          "id": 3,
-          "name": "Pam",
-          "date": "1/21",
-          "time": "6:00",
-          "number": 4
-          },
-      ]
+      reservations: [],
     }
   }
+
+  componentDidMount() {
+    getAllReservations()
+      .then(data => {
+        this.setState({reservations: data})
+      })
+  }
+
+
   render() {
     return (
       <div className="App">
