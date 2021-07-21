a5493e678047928ba746042fb34337112e898edb
Ashton Huxtable <aehuxtable@gmail.com>
2021-07-21 09:49:04 -0600

    Add function to render multiple reservations

--- src/components/Reservations/Reservations.js
+++ src/components/Reservations/Reservations.js
@@ -1,12 +1,27 @@
 import React from 'react';
+import ResCard from '../ResCard/ResCard';
 
 import './Reservations.css'
 
 const Reservations = ({reservations}) => {
     
-    // const allReservations = reservations.map()
+    const allReservations = reservations.map(reservation => {
+       return (
+           <ResCard 
+               name={reservation.name}
+               date={reservation.date}
+               time={reservation.time}
+               number={reservation.number}
+               id={reservation.id}
+               key={reservation.id}
+           />
+           ) 
+    })
     
     return (
+        <section className='all-reservations'>
+            {allReservations}
+        </section>
         
     )
 }
