47292256d1e08df70c12ac1ac270f4afe1c76f1e
Ashton Huxtable <aehuxtable@gmail.com>
2021-07-21 10:13:42 -0600

    Add state and inputs for form

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -5,12 +5,42 @@ import './Form.css';
 class Form extends Component {
     constructor() {
         super();
+        this.state = {
+            name: '',
+            date: '',
+            time: '',
+            number: ''
+        }
     }
 
     render(){
         return(
             <form>
-                
+                <input 
+                    type='text'
+                    placeholder='Name'
+                    name='name'
+                    value={this.state.name}
+                />
+                 <input 
+                    type='text'
+                    placeholder='Date (mm/dd'
+                    name='date'
+                    value={this.state.date}
+                />
+                 <input 
+                    type='text'
+                    placeholder='Time'
+                    name='time'
+                    value={this.state.time}
+                />
+                 <input 
+                    type='text'
+                    placeholder='Number of guests'
+                    name='number'
+                    value={this.state.number}
+                />
+                <button>Make Reservation</button>
             </form>
         )
     }
